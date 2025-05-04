633dfb83ca154b88c1f5b31e0838f719ae736eac
anvafi <anvafi@alumnatflorida.es>
2025-05-04 21:53:59 +0200

    pi009_intento

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,22 +6,22 @@ router.get("/", (req, res) => {
   console.log("estan dentro");
   res.send("hola");
 });
-router.get("/ruta", (req, res) => {
-  console.log("estan en ruta y tienen hambre");
-  res.send("¿en serio?");
-});
-router.get("/ruta/ruta", (req, res) => {
-  console.log("estan en descifrando el código");
-  res.send("tengo ganas de fumar :(");
-});
-router.get("/ruta/ruta/ruta", (req, res) => {
-  console.log("penetrando");
-  res.send("abre una botella de vino");
-});
-router.get("/ruta/ruta/ruta/ruta", (req, res) => {
-  console.log("fondo");
-  res.json("lleison");
-});
+// router.get("/ruta", (req, res) => {
+//   console.log("estan en ruta y tienen hambre");
+//   res.send("¿en serio?");
+// });
+// router.get("/ruta/ruta", (req, res) => {
+//   console.log("estan en descifrando el código");
+//   res.send("tengo ganas de fumar :(");
+// });
+// router.get("/ruta/ruta/ruta", (req, res) => {
+//   console.log("penetrando");
+//   res.send("abre una botella de vino");
+// });
+// router.get("/ruta/ruta/ruta/ruta", (req, res) => {
+//   console.log("fondo");
+//   res.json("lleison");
+// });
 router.post("/rutaPOST", (req, res) => {
   console.log("acceso a POST");
   res.send(`${req.body.nombre} , jesucristo murio por tus pecados`);
@@ -35,33 +35,33 @@ router.post("/rutaPOST", (req, res) => {
   //   fecha_caducidad: req.body.fecha_caducidad,
   //   cvv: req.body.cvv,
   // });
-  console.log(req.body);
-  console.log(req.body.nombre);
-  console.log(req.body.apellidos);
-  console.log(req.body.numero_tarjeta);
-  console.log(req.body.fecha_caducidad);
-  console.log(req.body.cvv);
+  // console.log(req.body);
+  // console.log(req.body.nombre);
+  // console.log(req.body.apellidos);
+  // console.log(req.body.numero_tarjeta);
+  // console.log(req.body.fecha_caducidad);
+  // console.log(req.body.cvv);
 });
-router.post("/rutaPOST/rutaPOST", (req, res) => {
-  console.log("acceso a POST");
-  res.send("hola, respuesta 2");
-  req.body = "test de request body";
-});
-router.post("/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
-  console.log("acceso a POST");
-  res.send("hola, respuesta 3");
-  req.body = "test de request body";
-});
-router.post("/rutaPOST/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
-  console.log("acceso a POST");
-  res.send("hola, respuesta 4");
-  req.body = "test de request body";
-});
-router.post("/rutaPOST/rutaPOST/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
-  console.log("acceso a POST");
-  res.send("hola, respuesta 5");
-  req.body = "test de request body";
-});
+// router.post("/rutaPOST/rutaPOST", (req, res) => {
+//   console.log("acceso a POST");
+//   res.send("hola, respuesta 2");
+//   req.body = "test de request body";
+// });
+// router.post("/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
+//   console.log("acceso a POST");
+//   res.send("hola, respuesta 3");
+//   req.body = "test de request body";
+// });
+// router.post("/rutaPOST/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
+//   console.log("acceso a POST");
+//   res.send("hola, respuesta 4");
+//   req.body = "test de request body";
+// });
+// router.post("/rutaPOST/rutaPOST/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
+//   console.log("acceso a POST");
+//   res.send("hola, respuesta 5");
+//   req.body = "test de request body";
+// });
 
 router.post("/rutaPOST_GTP", (req, res) => {
   const openai = new OpenAI({
