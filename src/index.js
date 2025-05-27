e5f1e27007a2d3b298debb61e2b54ae36710373e
anvafi <anvafi@alumnatflorida.es>
2025-05-27 12:33:46 +0200

    "inspiracion"

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 
-app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 5500);
 app.set("json spaces", 2);
 
 app.use(morgan("dev"));
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 //super rutas aquí
-// app.use(require("./routes/index"));
+//app.use(require("./routes/index"));
 app.use("/api/MySQL", require("./routes/index"));
 
 app.listen(app.get("port"), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,44 +1,44 @@
 const { Router } = require("express");
-const { default: OpenAI } = require("openai");
 const router = Router();
 const mysql = require("mysql2");
 
-router.get("/", (req, res) => {
-  console.log("estan dentro");
-  res.send("hola");
+//Create MySQL conexion
+const db = mysql.createConnection({
+  host: "fdb1028.awardspace.net",
+  user: "4633478_pi10",
+  password: "G32T2Nf]6&]?YBu",
+  database: "4633478_pi10",
 });
+db.connect((error) => {
+  if (error) {
+    console.log("error: " + error);
+  } else {
+    console.log("conexion establecida");
+  }
+});
+
+//GET
+router.get("/", (req, res) => {
+  res.send("hola desde SQLLandia");
+});
+
+//GET
 router.get("/getSQL", (req, res) => {
-  console.log(db);
-  let sql = "SELECT * FROM usuario";
+  // console.log(db);
+  let sql = "SELECT * FROM usuarios";
   let query = db.query(sql, (err, result) => {
     console.log(result);
     res.json(result);
   });
 });
-// router.get("/ruta", (req, res) => {
-//   console.log("estan en ruta y tienen hambre");
-//   res.send("¿en serio?");
-// });
-// router.get("/ruta/ruta", (req, res) => {
-//   console.log("estan en descifrando el código");
-//   res.send("tengo ganas de fumar :(");
-// });
-// router.get("/ruta/ruta/ruta", (req, res) => {
-//   console.log("penetrando");
-//   res.send("abre una botella de vino");
-// });
-// router.get("/ruta/ruta/ruta/ruta", (req, res) => {
-//   console.log("fondo");
-//   res.json("lleison");
-// });
-router.post("/rutaPOST", (req, res) => {
-  console.log("acceso a POST");
-  res.send(`${req.body.nombre} , jesucristo murio por tus pecados`);
-});
+
+//POST
 router.post("/postSQL", (req, res) => {
+  console.log("hola");
+  // let post = { usuario: "usuarioNODE", pass: 54321 }; //Valores de testeo, se sustituyen por el contenido del POST
   let post = req.body;
   let sql =
-    "INSERT INTO `usuario` (`usuario`, `pass`) VALUES ('" +
+    "INSERT INTO `usuarios` (`usuario`, `pass`) VALUES ('" +
     req.body.usuario +
     "', '" +
     req.body.pass +
@@ -51,77 +51,6 @@ router.post("/postSQL", (req, res) => {
       res.json(result);
     }
   });
-});
-//req.body = "test de request body";
-//res.redirect("http://127.0.0.1:5500/frondend/index.html");
-// res.json({
-//   id: "dinerico",
-//   nombre: req.body.nombre,
-//   apellidos: req.body.apellidos,
-//   numero_tarjeta: req.body.numero_tarjeta,
-//   fecha_caducidad: req.body.fecha_caducidad,
-//   cvv: req.body.cvv,
-// });
-// console.log(req.body);
-// console.log(req.body.nombre);
-// console.log(req.body.apellidos);
-// console.log(req.body.numero_tarjeta);
-// console.log(req.body.fecha_caducidad);
-// console.log(req.body.cvv);
-//});
-// router.post("/rutaPOST/rutaPOST", (req, res) => {
-//   console.log("acceso a POST");
-//   res.send("hola, respuesta 2");
-//   req.body = "test de request body";
-// });
-// router.post("/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
-//   console.log("acceso a POST");
-//   res.send("hola, respuesta 3");
-//   req.body = "test de request body";
-// });
-// router.post("/rutaPOST/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
-//   console.log("acceso a POST");
-//   res.send("hola, respuesta 4");
-//   req.body = "test de request body";
-// });
-// router.post("/rutaPOST/rutaPOST/rutaPOST/rutaPOST/rutaPOST", (req, res) => {
-//   console.log("acceso a POST");
-//   res.send("hola, respuesta 5");
-//   req.body = "test de request body";
-// });
-
-router.post("/rutaPOST_GPT", (req, res) => {
-  const openai = new OpenAI({
-    apiKey:
-      "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",
-  });
-  const completion = openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    store: true,
-    messages: [
-      {
-        role: "developer",
-        content: "Pregunta" + req.body.pregunta,
-      },
-    ],
-  });
-  completion.then((result) => {
-    res.send(result.choices[0]);
-  });
-});
-
-const db = mysql.createConnection({
-  host: "fdb1028.awardspace.net",
-  user: "4633478_pi10",
-  password: "G32T2Nf]6&]?YBu",
-  database: "4633478_pi10",
-});
-db.connect((error) => {
-  if (error) {
-    console.log("error" + error);
-  } else {
-    console.log("conexión establecida");
-  }
 });
 
 module.exports = router;
